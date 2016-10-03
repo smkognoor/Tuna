5dd489ec7eb7f5dbbe76b4f259bb25ede9c30f18
shashikumar <shashi.kognoor@gmail.com>
2016-10-03 21:21:37 +0530

    this is my second

---
+++ EXCELLSHEET/src/com/system/ExcelSheetConfigue.java
@@ -0,0 +1,58 @@
+package com.system;
+
+
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelSheetConfigue {
+	public XSSFWorkbook wb;
+	
+	public XSSFSheet Sheet1;
+	
+	public ExcelSheetConfigue(String ExcelSheet){
+		
+		
+		try{
+			
+			File  src=new File(ExcelSheet);
+			
+			FileInputStream  fis=new FileInputStream(src);
+			
+			  wb=new XSSFWorkbook(fis);
+			
+			
+		}
+		catch(Exception e){
+			
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
+	
+	
+	public String getData(int SheetNumber, int Rows, int col){
+		
+	Sheet1=wb.getSheetAt(SheetNumber);
+	
+	String data=Sheet1.getRow(Rows).getCell(col).getStringCellValue();
+	
+	return data;
+		
+	}
+	
+	public int getRowCount(int SheetIndex){
+		
+		int Row=wb.getSheetAt(SheetIndex).getLastRowNum();
+		
+	Row=Row+1;
+	
+	return Row;
+	}
+		
+	}
+	
