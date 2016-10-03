019e7ea1b1329b7a831a2b59b32079ccd246bbb7
shashikumar <shashi.kognoor@gmail.com>
2016-10-03 22:02:12 +0530

    this is ffinal

--- EXCELLSHEET/src/com/system/simple.java
+++ EXCELLSHEET/src/com/system/simple.java
@@ -6,10 +6,9 @@ public class simple {
 	public static void main(String args[]){
 		
 		System.out.println("hello Shashi");
-		
 		System.out.println("hello sinior");
-		
 		System.out.println("hi malleshe");
+		System.out.println("namaste Shashi");
 	}
 
 }
